9904f08d9f0efd7288bfc844e6999ef3a4d6c4ed
chirag <chiragmahajan9112@gmail.com>
2025-02-24 17:44:47 +0530

    new update1

--- module_main.tf
+++ module_main.tf
@@ -16,6 +16,6 @@ module "my_instance"{
     instance_type = var.instance_type
     key_name = var.key_name
     sgid = var.sgid
-    subnet_id = module.my_vpc.aws_subnet.my_subnet.id
+    subnet_id = module.my_vpc.my_subnet.id
 
 }
